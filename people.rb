b26ef63c3c07a06eaed2f71a0c4cc8d58a0d0dbb
adamrivietz <adam_riv@hotmail.com>
2014-01-16 15:38:52 -0500

    Exercise1

--- people.rb
+++ people.rb
@@ -1,17 +1,32 @@
 class Person
-	def initialize
+	def initialize (name)
 		@name = name
 	end
+	def greeting
+		puts "Hi, my name is #{@name}"
+	end	
 end
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object!"
 	end
 end
+
+Chris = Instructor.new("Chris")
+Chris.greeting
+
+Cristina = Student.new("Cristina")
+Cristina.greeting
+
+Chris.teach
+Cristina.learn
+
+#Christina.teach
+#Does not work because Christina can only perform actions assigned to the Student/Person class, not the Instructor class.
